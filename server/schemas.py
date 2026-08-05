d9cb76724015d53d4227a997eb3c56f53b67b48f
CrescentTime <177346196+CrescentTime@users.noreply.github.com>
2026-08-05 16:54:17 +0300

    Added email to user schema

--- server/schemas.py
+++ server/schemas.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 class CreateUser(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     password: str = Field(min_length=1, max_length=50)
+    email: str = Field(min_length=1)
 
 
 class ReadUser(BaseModel):
