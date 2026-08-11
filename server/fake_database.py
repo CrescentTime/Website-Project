0c5aa5ba9eeeaf252c1537efaf2f400416d5734f
CrescentTime <177346196+CrescentTime@users.noreply.github.com>
2026-08-11 14:22:31 +0300

    Reformating the database

--- server/fake_database.py
+++ server/fake_database.py
@@ -1,14 +1,32 @@
 users = \
-    {1: [      # id
-        'rex', # 0 name
-        'a',   # 1 password
-        [1],   # 2 wishlist
-        [],    # 3 cart
-        {},    # 4 reviews
-        [],    # 5 purchases
-        'rex23@email.com' # 6 email
-    ]} # id : [name, password, wishlist, cart, review, purchases, email]
+    {
+        1: [                 # id
+            'rex',            # 0 name
+            'a',              # 1 password
+            [],               # 2 wishlist
+            [],               # 3 cart
+            {1: 'great'},     # 4 reviews (product id, review)
+            [1],              # 5 purchases
+            'rex23@email.com' # 6 email
+            ],
+        2: [
+            'Bash',
+            'red3',
+            [1],
+            [],
+            {},
+            [],
+            'rex23@email.com'
+        ]
+    } # id : [name, password, wishlist, cart, review, purchases, email]
 
-products = {1: ['appple', {1: 'great'}]}
+products = {
+    1: [                            # product id
+        'The Talos Principle',      # Game name
+        {
+            1: 'great'              # User id, review
+        }
+    ]
+}
 
 password_reset_tokens = {}
