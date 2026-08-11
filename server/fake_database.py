12382e377568d5050aad8e932a54c705ed4ad789
CrescentTime <177346196+CrescentTime@users.noreply.github.com>
2026-08-11 14:00:40 +0300

    Created fake database

---
+++ server/fake_database.py
@@ -0,0 +1,14 @@
+users = \
+    {1: [      # id
+        'rex', # 0 name
+        'a',   # 1 password
+        [1],   # 2 wishlist
+        [],    # 3 cart
+        {},    # 4 reviews
+        [],    # 5 purchases
+        'rex23@email.com' # 6 email
+    ]} # id : [name, password, wishlist, cart, review, purchases, email]
+
+products = {1: ['appple', {1: 'great'}]}
+
+password_reset_tokens = {}
