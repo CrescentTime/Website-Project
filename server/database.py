29f106c919c579945daa097db3749ed76a9b913b
CrescentTime <177346196+CrescentTime@users.noreply.github.com>
2026-08-24 19:46:58 +0300

    Added dotenv library

--- server/database.py
+++ server/database.py
@@ -1,12 +1,16 @@
 import os
 from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 url = URL.create(
     "postgresql+psycopg2",
     username=os.getenv("USER"),
     password=os.getenv("PASSWORD"),
     host=os.getenv("HOST"),
+    port=os.getenv("PORT"),
     database=os.getenv("DATABASE_NAME")
 )
 
